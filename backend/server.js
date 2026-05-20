f0dced32f105d48b00dff25e89b6d7c432bdc910
Hari Krishna <harikrishnag41045@gmail.com>
2026-05-20 23:15:13 +0530

    configured production deployment

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,28 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+//     credentials: true,
+// }));
+// app.use(
+//   cors({
+//     origin: [
+//       'http://localhost:5173',
+//       'http://localhost:5174',
+//       'https://devvpulse.online',
+//     ],
+//     credentials: true,
+//   })
+// );
+
+
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+
 
 app.use(express.json());
 
